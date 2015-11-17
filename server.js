877d71c18b47495567653f777ee628df79d274f0
Blake Hagen <blakehagen@gmail.com>
2015-11-17 12:47:34 -0700

    done

--- server.js
+++ server.js
@@ -4,21 +4,48 @@ var cors = require('cors');
 var mongojs = require('mongojs');
 
 var app = express();
-app.use(bodyParser());
+
+app.use(bodyParser.json());
+app.use(cors());
 
 var db = mongojs('birds');
-var Bird = db.collection('sightings');
+var sightings = db.collection('sightings');
+
+app.get('/api/sighting', function (req, res, next) {
+    sightings.find(req.query, function (err, result) {
+        if (err) res.sent(err);
+        else res.json(result);
+    })
+});
+
+
+app.post('/api/sighting', function (req, res, next) {
+    sightings.insert(req.body, function (err, result) {
+        if (err) res.sent(err);
+        else res.json(result);
+    })
+});
+
+
+app.put('/api/sighting', function (req, res, next) {
+    sightings.update({ "_id": mongojs.ObjectId(req.query.id) }, req.body, function (err, result) {
+        if (err) res.sent(err);
+        else res.json(result);
+    })
+});
 
 
 
+app.delete('/api/sighting', function (req, res, next) {
+    sightings.remove({ " _id": mongojs.ObjectId(req.query.id) }, function (err, result) {
+        if (err) res.sent(err);
+        else res.json(result);
+    })
+});
 
 
 
-
-
-
-
-
+// listen to port
 app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
